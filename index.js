3336002d03421ba9b851bcb28c6d7f1d600a1f4e
Corbie2 <evgeny.bratchikov@optimax.dev>
2022-10-25 11:33:31 +0300

    v1 release

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ async function run() {
     const ms = core.getInput('milliseconds');
     core.info(`Waiting ${ms} milliseconds ...`);
 
+    const repoOwner = github.context.repo.owner
+    const repo = github.context.repo.repo
+
+    let client = github.getOctokit(core.getInput('token'))
+    let resp = client.rest.pulls.list({
+      owner: repoOwner,
+      repo: repo,
+    }).catch(
+      e => {
+        core.setFailed(e.message)
+      }
+    );
+
+    core.debug(resp);
+
     core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     await wait(parseInt(ms));
     core.info((new Date()).toTimeString());
