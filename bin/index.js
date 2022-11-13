83c8a45081766b2d4321932bfcc0db603642df68
Corbie2 <evgeny.bratchikov@optimax.dev>
2022-11-13 12:15:59 +0400

    test

--- bin/index.js
+++ bin/index.js
@@ -11982,6 +11982,7 @@ const octokit = new Octokit({ auth: token });
 const repoOwner = github.context.repo.owner
 const repo = github.context.repo.repo
 const baseBranch = github.context.payload.ref
+let pullRequestArray = [];
 
 const getPullRequests = async () => {
     const resp = octokit.rest.pulls.list({
@@ -11997,51 +11998,44 @@ const getPullRequests = async () => {
     return resp;
 };
 
-const updateBranch = async (filteredPrs) => {
+const updateBranch = async () => {
     if (github.context.ref === `refs/heads/${baseBranch}`) {
         return {
             type: 'warning',
             msg: 'Commit is already on the destination branch, ignoring',
         };
     }
-
-    filteredPrs.some(async (pr) => {
-        console.log('ref', github.context.ref);
-        try {
-            // await octokit.request(
-            //     'PUT /repos/{owner}/{repo}/pulls/{pull_number}/update-branch',
-            //     {
-            //         owner: repoOwner,
-            //         repo: repo,
-            //         pull_number: pr.number,
-            //     }
-            // )
-            await octokit.rest.pulls.updateBranch({
-                owner: repoOwner,
-                repo: repo,
-                pull_number: pr.number,
-            }).then(() => {
-                console.log('updated', pr.number);
-            });
-        } catch (error) {
-            console.warn('error', error);
-        }
-    })
-    
+    console.log(pullRequestArray);
+    console.log('****************');
+    try {
+        await octokit.rest.pulls.updateBranch({
+            owner: repoOwner,
+            repo: repo,
+            pull_number: pr.number,
+        }).then(() => {
+            console.log('updated', pr.number);
+        });
+    } catch (error) {
+        pullRequestArray.shift();
+        updateBranch();
+        console.warn('error', error);
+    };
 };
 
 async function main() {
     const pullRequestsList = await getPullRequests();
     const filteredPrs = pullRequestsList.data.filter((pr) => pr.auto_merge !== null);
 
+    pullRequestArray = filteredPrs;
+
     filteredPrs.map((pr) => { console.log(`Pull Request - ${pr.number} ${pr.created_at}`)})
 
-    if (!filteredPrs.length) {
+    if (!pullRequestArray.length) {
         console.log('auto-merge prs is not found');
         return
     }
-    if (filteredPrs.error) console.log(filteredPrs.error);  
-    updateBranch(filteredPrs);
+    if (pullRequestArray.error) console.log(pullRequestArray.error);  
+    updateBranch();
 };
 
 main();
